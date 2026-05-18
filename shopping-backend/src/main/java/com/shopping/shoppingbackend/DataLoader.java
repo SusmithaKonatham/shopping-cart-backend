d3be22e5c770b9c9961cec1377d0077a7fd83b11
SusmithaKonatham <78208308+SusmithaKonatham@users.noreply.github.com>
2026-05-18 15:34:38 +0530

    data changes

--- shopping-backend/src/main/java/com/shopping/shoppingbackend/DataLoader.java
+++ shopping-backend/src/main/java/com/shopping/shoppingbackend/DataLoader.java
@@ -19,21 +19,21 @@ public class DataLoader {
             Product product1 = new Product();
             product1.setName("iPhone 15");
             product1.setPrice(new BigDecimal("80000"));
-            product1.setImageUrl("https://images.unsplash.com/photo-1511707171634-5f897ff02aa9");
+            product1.setImageUrl("https://images.unsplash.com/photo-1592750475338-74b7b21085ab");
             product1.setActive(true);
             repository.save(product1);
 
             Product product2 = new Product();
             product2.setName("Laptop");
             product2.setPrice(new BigDecimal("70000"));
-            product2.setImageUrl("https://images.unsplash.com/photo-1496181133206-80ce9b88a853");
+            product2.setImageUrl("https://images.unsplash.com/photo-1498050108023-c5249f4df085");
             product2.setActive(true);
             repository.save(product2);
 
             Product product3 = new Product();
             product3.setName("Samsung TV");
             product3.setPrice(new BigDecimal("55000"));
-            product3.setImageUrl("https://images.unsplash.com/photo-1593784991095-a205069470b6");
+            product3.setImageUrl("https://images.unsplash.com/photo-1461151304267-38535e780c79");
             product3.setActive(true);
             repository.save(product3);
         };
